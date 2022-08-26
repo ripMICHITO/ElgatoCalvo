b3dd7223213932305f1adc4eb8b9a52610e54d30
ripMICHITO <101848888+ripMICHITO@users.noreply.github.com>
2022-08-25 22:30:40 -0500

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,7 @@
+addRoom(){document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+    Room_names = childKey;
+    //Inicia código
+    <div> </div>
+    //Termina código
+    });});}
+    getData();
